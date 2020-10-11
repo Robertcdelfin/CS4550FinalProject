01635566d41aff7b620dfdc2ec55d0732e6d4d21
Robertcdelfin <42871949+Robertcdelfin@users.noreply.github.com>
2020-10-10 17:43:57 -0700

    Create vector3Float.java
    a class to hold the x y z vectors

---
+++ vector3Float.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw3cs4550;
+
+/**
+ *
+ * Purpse: Holds the x y z values in 3d vecctor space
+ */
+public class Vector3Float {
+    public float x,y,z;
+    public Vector3Float(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
