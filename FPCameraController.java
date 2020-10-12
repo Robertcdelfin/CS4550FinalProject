3d1661c325e92695e6c177e713df51b281223265
Robertcdelfin <42871949+Robertcdelfin@users.noreply.github.com>
2020-10-12 13:34:46 -0700

    Update FPCameraController.java
    Team update to the class

--- FPCameraController.java
+++ FPCameraController.java
@@ -3,72 +3,251 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hw3cs4550;
+package hw3cs4450;
+
+
 import org.lwjgl.util.vector.Vector3f;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.Sys;
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
 
 /**
- *PUrpose: The class FP CameraControl is the first person controller
+ *Purpose: The class FP CameraControl is the first person controller
  */
-public class FPCameraController {
+public class FPCameraController 
+{
     
     //2d vector to store camer psoition
     private Vector3f position = null;
-    private Vector3f positionI = null;
-          
+    private Vector3f lPosition = null;
+    
     //rotation around Y axis of camer
-    private float yaw = 0.0f;
+    private float yaw = 0f;
+    
     //roation around X axis of the camera
-    private float pitch = 0.0f;
-    private Vector3Float me;
-    
+    private float pitch = 0f;
+    private Vector3f camera;
+
     //The instatiation of positon and xyz
-    public FPCameraController(float x, float y, float z){
-        //instatinte position Vector 3f to the xyz 
-        position = new Vector3f(x,y,z);
-        positionI = new Vector3f(x,y,z);
-        positionI.x = 0f;
-        positionI.y = 15f;
-        positionI.z = 0f;
+    public FPCameraController(float x, float y, float z) 
+    { 
+        //instantiate position Vector3f to the x y z params.
+        position = new Vector3f(x, y, z);
+        lPosition = new Vector3f(x,y,z);
+        lPosition.x = 0f;
+        lPosition.y = 200f;
+        lPosition.z = 0f;
+
     }
+
+   //increment the camera's current yaw rotation
+   public void yaw(float amount) {
+       //increment the yaw by the amount param
+       yaw += amount;
+   }
     //increment the camera's current yaw rotation
-    public void yaw(float amount){
-        yaw += amount;
+    public void pitch(float amount) {
+        //increment the pitch by the amount param
+        pitch -= amount;
     }
-    
-    //increment the camera's current yaw rotaiton.
-    public void pitch(float amount){
-        //increment the pitch  by the amount param
-        pitch -=amount;
-    }
-    //moves the cmaer forward relative to current rotaiton
-    public void walkForward(float distance){
-        float xOffset = distance*(float)Math.sin(Math.toRadians(yaw));
-        float zOffset = distance*(float)Math.cos(Math.toRadians(yaw));
+    //moves the camera forward relative to its current rotation (yaw)
+    public void walkForward(float distance) 
+    {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
         position.x -= xOffset;
         position.z += zOffset;
+        FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(lPosition.x-=xOffset).put(lPosition.y).put(lPosition.z+=zOffset).put(1.0f).flip();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);
     }
-    public void walkBackwards(float distance){
-        float xOffset = distance*(float)Math.sin(Math.toRadians(yaw));
-        float zOffset = distance*(float)Math.cos(Math.toRadians(yaw));
+
+    //moves the camera backward relative to its current rotation (yaw)
+    public void walkBackwards(float distance) 
+    {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw));
         position.x += xOffset;
         position.z -= zOffset;
+        FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(lPosition.x+=xOffset).put(lPosition.y).put(lPosition.z-=zOffset).put(1.0f).flip();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);
     }
-    public void strafeLeft(float distance){
-        float xOffset = distance*(float)Math.sin(Math.toRadians(yaw-90));
-        float zOffset = distance*(float)Math.cos(Math.toRadians(yaw-90));
+
+    //strafes the camera left relative to its current rotation (yaw)
+    public void strafeLeft(float distance) 
+    {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw-90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw-90));
         position.x -= xOffset;
         position.z += zOffset;
+        FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(lPosition.x-=xOffset).put(lPosition.y).put(lPosition.z+=zOffset).put(1.0f).flip();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);
     }
-    public void strafeRight(float distance){
-        float xOffset = distance*(float)Math.sin(Math.toRadians(yaw+90));
-        float zOffset = distance* (float)Math.cos(Math.toRadians(yaw+90));
+
+    //strafes the camera right relative to its current rotation (yaw)
+    public void strafeRight(float distance) 
+    {
+        float xOffset = distance * (float)Math.sin(Math.toRadians(yaw+90));
+        float zOffset = distance * (float)Math.cos(Math.toRadians(yaw+90));
         position.x -= xOffset;
-        position.z += zOffset; 
+        position.z += zOffset;
+        FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(lPosition.x-=xOffset).put(lPosition.y).put(lPosition.z+=zOffset).put(1.0f).flip();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);
+    }
+    //moves the camera up relative to its current rotation (yaw)
+    public void moveUp(float distance) {
+        position.y -= distance;
+    }
+    //moves the camera down
+    public void moveDown(float distance) {
+        position.y += distance;
+    }
+
+    //translates and rotate the matrix so that it looks through the camera
+    //this does basically what gluLookAt() does
+    public void lookThrough() {
+        //roatate the pitch around the X axis
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        //roatate the yaw around the Y axis
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        //translate to the position vector's location
+        glTranslatef(position.x, position.y, position.z);
+        
+        FloatBuffer lightPosition = BufferUtils.createFloatBuffer(4);
+        lightPosition.put(lPosition.x).put(lPosition.y).put(lPosition.z).put(1.0f).flip();
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);
     }
     
+    // keep the game session until user presses ESC
+    public void gameLoop() {
+        FPCameraController camera = new FPCameraController(0, 0, 0);
+        float dx = 0.0f;
+        float dy = 0.0f;
+        float dt = 0.0f; //length of frame
+        float lastTime = 0.0f; // when the last frame was
+        long time = 0;
+        float mouseSensitivity = 0.09f;
+        float movementSpeed = .35f;
+        //hide the mouse
+        Mouse.setGrabbed(true); 
+        // keep looping till the display window is closed the ESC key is down
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            time = Sys.getTime();
+            lastTime = time;
+            //distance in mouse movement
+            //from the last getDX() call.
+            dx = Mouse.getDX();
+            //distance in mouse movement
+            //from the last getDY() call.
+            dy = Mouse.getDY();
+            //when passing in the distance to move
+            //we times the movementSpeed with dt this is a time scale
+            //so if its a slow frame u move more then a fast frame
+            //so on a slow computer you move just as fast as on a fast computer
+            camera.yaw(dx * mouseSensitivity);
+            camera.pitch(dy * mouseSensitivity);
+            if (Keyboard.isKeyDown(Keyboard.KEY_W))//move forward          
+                camera.walkForward(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_S))//move backward            
+                camera.walkBackwards(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_A))//strafe left            
+                camera.strafeLeft(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_D))//strafe right            
+                camera.strafeRight(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE))//move up            
+                camera.moveUp(movementSpeed);           
+            if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))  
+                camera.moveDown(movementSpeed);               
+            if (Keyboard.isKeyDown(Keyboard.KEY_E)) // for screenshot purposes
+                Mouse.setGrabbed(false);     
+            if (Keyboard.isKeyDown(Keyboard.KEY_R))  
+                Mouse.setGrabbed(true);  
+            //set the modelview matrix back to the identity
+            glLoadIdentity();
+            //look through the camera before you draw anything
+            camera.lookThrough();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            render();
+            //you would draw your scene here.
+  
+            //draw the buffer to the screen
+            Display.update();
+            Display.sync(60);
+        }
+        Display.destroy();
+    }
+
+    // draw the cube (outdated) holds the data for the shape.
+    private void render() 
+    {
+        try
+        {
+            
+            // Top
+            glColor3f(1f, 0f, 1f);
+            glBegin(GL_QUADS);
+            glVertex3f(1f, 1f, -1f);
+            glVertex3f(-1f, 1f, -1f);
+            glVertex3f(-1f, 1f, 1f);
+            glVertex3f(1f, 1f, 1f);
+            glEnd();
+        
+            // Bottom
+            glColor3f(1f, 1f, 0f);
+            glBegin(GL_QUADS);
+            glVertex3f(1f, -1f, 1f);
+            glVertex3f(-1f, -1f, 1f);
+            glVertex3f(-1f, -1f, -1f);
+            glVertex3f(1f, -1f, -1f);
+            glEnd();
+
+            // Front
+            glColor3f(1f, 0f, 0f);
+            glBegin(GL_QUADS);
+            glVertex3f(1f, 1f, 1f);
+            glVertex3f(-1f, 1f, 1f);
+            glVertex3f(-1f, -1f, 1f);
+            glVertex3f(1f, -1f, 1f);
+            glEnd();
+        
+            // Back
+            glColor3f(0f, 0f, 1f);
+            glBegin(GL_QUADS);
+            glVertex3f(1f, -1f, -1f);
+            glVertex3f(-1f, -1f, -1f);
+            glVertex3f(-1f, 1f, -1f);
+            glVertex3f(1f, 1f, -1f);
+            glEnd();  
+            
+            // Left
+            glColor3f(0f, 1f, 0f);
+            glBegin(GL_QUADS);
+            glVertex3f(-1f, 1f, 1f);
+            glVertex3f(-1f, 1f, -1f);
+            glVertex3f(-1f, -1f, -1f);
+            glVertex3f(-1f, -1f, 1f);
+            glEnd();
+        
+            // Right
+            glColor3f(0f, 1f, 1f);
+            glBegin(GL_QUADS);
+            glVertex3f(1f, 1f, -1f);
+            glVertex3f(1f, 1f, 1f);
+            glVertex3f(1f, -1f, 1f);
+            glVertex3f(1f, -1f, -1f);
+            glEnd();
+            
+        } 
+        catch (Exception e) 
+        {
+            
+        }
+    }
 }
